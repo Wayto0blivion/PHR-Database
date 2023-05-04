15eeec77a096ed1361dc106b8050e553645f1c00
Wayto0blivion <98188354+Wayto0blivion@users.noreply.github.com>
2023-05-04 11:26:27 -0400

    Removed commented code from views and searchviews for a cleaner reading experience.

--- services/flask/website/searchviews.py
+++ services/flask/website/searchviews.py
@@ -11,71 +11,6 @@ searchviews = Blueprint('searchviews', __name__)
 ROWS_PER_PAGE = 50
 
 
-# @searchviews.route('/search_test', methods=['GET', 'POST'])
-# def searchtest():
-#     """
-#     Gives search results for partial searches.
-#     Currently, only checks for partial searches on "orderNo" column
-#
-#     use getattr to get the attribute of Production (like orderNo) to call the results from select_field
-#     """
-#     form = ProductionSearchForm(request.form)
-#     if request.method == 'POST':
-#         select_field  = form.select.data
-#         #print(select_field)
-#         search_string = form.search.data
-#         #print(search_string,select_field)
-#         if search_string == '':
-#             results = db.session.query(Production).limit(5000)
-#         else:
-#             kwargs = {str(select_field):str(search_string)}
-#             results = Production.query.filter(Production.orderNo.like(**kwargs)).all()
-#
-#         if not results:
-#             flash('No results found!', category='error')
-#             print('if not: ' + form)
-#             return redirect(url_for('.searchtest', form=form))
-#         else:
-#             return render_template('search.html', form=form, result=results, user=current_user)
-#
-#
-#
-#     else:
-#     #    return redirect(url_for('.searchtest', form=form, user=current_user))
-#         return render_template('search.html', form=form, user=current_user)
-
-
-
-#Testing. For basic search methods. Paired with below
-# @searchviews.route('/search', methods=['GET', 'POST'])
-# def search_func():
-#     '''
-#
-#     '''
-#     search = ProductionSearchForm(request.form)
-#     if request.method == 'POST':
-#         return search_results(search)
-#
-#     return render_template('search.html', form=search, user=current_user)
-
-# @searchviews.route('/searchresults')
-# def search_results(search):
-#     results = []
-#     search_string = search.data['search']
-#     print("search_string: " + search_string)
-#
-#     if search.data['search'] == '':
-#         qry = db.session.query(Production)
-#         results = qry.limit(5000)
-#
-#     if not results:
-#         flash('No results found', category='error')
-#         return redirect(url_for('.search_func', form=search, user=current_user))
-#     else:
-#         #display results
-#         return render_template('search.html', form=search, result=results, user=current_user)
-
-
 @searchviews.route('/hdd_search', methods=['GET', 'POST'])
 @login_required
 @hf.user_permissions('PC Tech')
@@ -123,111 +58,9 @@ def hdd_search():
             '''
             return hf.download_search(result_query, 'hddEngine')
 
-            # df = pandas.read_sql(result_query.statement, con=hddEngine.connect())
-            #
-            # resp = make_response(df.to_csv(index=False))
-            # resp.headers["Content-Disposition"] = "attachment; filename=Production_Export.csv"
-            # resp.headers["Content-Type"] = "text/csv"
-            # return resp
-
-            # return hf.download_hdd_search(result_query)
-
         return render_template('hdd_search.html', form=form, pagination=cur_result, count=count, quality_count=quality_count, user=current_user)
 
     return render_template('hdd_search.html', form=form, user=current_user)
-
-
-'''
-@searchviews.route('/pandas_search', methods=['GET', 'POST'])
-@login_required
-def pandasSearch():
-    prodForm = ProductionSearchForm()
-    if request.method == 'POST':
-        form = request.form
-        search_term = form['search']
-        select_term = form['select']
-
-        #dbConnection = sqlEngine.connect()
-        #sql_query = 'SELECT `Unit #` AS "Unit #",`Product Name` AS "Product Name", "NA" AS "HDD MFG", "NA" AS "HDD Serial #", `Manufacturer` AS "Manufacturer", `Model` AS "Model", `Serial Number` AS "Serial #", `Asset` AS "Asset", `Year Manufactured` AS "Year Manufactured", `Processor` AS Processor, `Speed` AS Speed, `RAM (GB)` AS RAM, `Media` AS Media, `COA` AS COA, `Form Factor` AS "Form Factor", "N/A" AS "HDD (GB)", `Test Result Codes` AS "TEST RESULT CODES (SEE TRANSLATIONS)", `Sale Category` AS "Sale Category" FROM Production'
-
-        #df = pandas.read_sql(sql_query, dbConnection)
-        #if select_term == 'orderNo':
-        #    select_term = 'Order Number'
-        #elif select_term == 'productName':
-        #    select_term = 'Product Name'
-        #elif select_term == 'serialNo':
-        #    select_term = 'Serial Number'
-        #elif select_term == 'date':
-        #    select_term = 'Date'
-
-        #formats string so that it can be passed to filter
-        #print(select_term)
-        #f = getattr(Production, select_term)
-        #search_format = {select_term: search_term}
-        #results_str = str(Production.query.filter_by(**search_format).all())
-        #result_builder = 'SELECT Production.* FROM Production WHERE `{0}` LIKE "%{1}%"'.format(select_term, search_term)
-        #results = Production.query.filter_by(**search_format).all()
-
-
-
-
-        results = Production.query.filter(getattr(Production, select_term).like('%{}%'.format(search_term)))
-        #print(str(results.statement))
-        #df_from_results = pandas.read_sql(Production.query.filter(getattr(Production, select_term).like('%{}%'.format(search_term))).statement, dbConnection).to_html(classes='table table-dark')
-        #results_df_html = df_from_results.to_html(classes='table')
-        #print(result_builder)
-        #results = result_builder
-        #results = db.session.execute(result_builder)
-        #print(results_str)
-        #results_string = str(results)
-        #print(str(results))
-        #df = pandas.read_sql(results.statement, dbConnection)
-        #mystring  = results.sub(r"&lrm;", "", results)
-        #mystring  = results.sub(r"&zwnj;", "", results)
-        #df_html = df.to_html(classes='table')
-        #print(str(results))
-        #if prodForm.validate_on_submit():
-        #    print('validated!')
-        #result_from_records = pandas.DataFrame.from_records(results)
-        #dbConnection = sqlEngine.connect()
-        #result_from_records = pandas.read_sql_query(results.statement, dbConnection)
-        #dbConnection.close()
-        #result_from_records = result_from_records.to_html(classes='table table-dark')
-
-
-
-        if prodForm.downl.data:
-            print('Download!')
-            if select_term == 'orderNo':
-                select_term = 'Order Number'
-            elif select_term == 'productName':
-                select_term = 'Product Name'
-            elif select_term == 'serialNo':
-                select_term = 'Serial Number'
-            elif select_term == 'date':
-                select_term = 'Date'
-
-            query_sets = db.session.execute('SELECT `Unit #` AS "Unit #",`Product Name` AS "Product Name", "NA" AS "HDD MFG", "NA" AS "HDD Serial #", `Manufacturer` AS "Manufacturer", `Model` AS "Model", `Serial Number` AS "Serial #", `Asset` AS "Asset", `Year Manufactured` AS "Year Manufactured", `Processor` AS Processor, `Speed` AS Speed, `RAM (GB)` AS RAM, `Media` AS Media, `COA` AS COA, `Form Factor` AS "Form Factor", "N/A" AS "HDD (GB)", `Test Result Codes` AS "TEST RESULT CODES (SEE TRANSLATIONS)", `Sale Category` AS "Sale Category" FROM Production WHERE `{0}` LIKE "%{1}%"'.format(select_term, search_term))
-            column_names = ["Unit #", 'Product Name', "HDD MFG", "HDD Serial #", 'Manufacturer', 'Model', 'Serial #', "Asset", 'Year Manufactured', 'Processor', 'Speed', 'RAM', 'Media', 'COA', 'Form Factor', 'HDD (GB)', "TEST RESULT CODES (SEE TRANSLATIONS)", 'Sale Category']
-            return excel.make_response_from_query_sets(query_sets, column_names, "csv")
-        else:
-            print('No Download!')
-
-
-        result_count = f"Search returned {results.count()} results"
-
-
-
-        return render_template('search.html', form=prodForm, results=results, count=result_count, user=current_user)
-    else:
-        return render_template('search.html', form=prodForm, user=current_user)
-'''
-
-
-#def query_db(select, search):
-#    f = getattr(Production, select)
-#    q = Production.query.filter((select))
-#    return q.all()
 
 
 @searchviews.route('/pandas_search', methods=['GET', 'POST'])
@@ -255,13 +88,6 @@ def pandasSearch():
         if form.downl.data:
 
             return hf.download_search(result_query, 'sqlEngine')
-
-            # df = pandas.read_sql(result_query.statement, con=sqlEngine.connect())
-            #
-            # resp = make_response(df.to_csv(index=False))
-            # resp.headers["Content-Disposition"] = "attachment; filename=Production_Export.csv"
-            # resp.headers["Content-Type"] = "text/csv"
-            # return resp
 
         return render_template('search.html', form=form,
                                pagination=cur_result, count=count, user=current_user)
@@ -309,6 +135,7 @@ def master_validation():
     return render_template('master_validation_table.html', form=form, user=current_user)
 
 # ----------------------------------------------------------------------------------------------------------------------
+
 
 @searchviews.route('hdd_validation_table', methods=['GET', 'POST'])
 @login_required
