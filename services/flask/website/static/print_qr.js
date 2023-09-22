6ccf032137a71380bc86021cacbfd8025e23f852
Mekayla <mekaylastapletonwork@gmail.com>
2023-09-22 14:14:14 -0400

    Added QR search function and related HTML.

--- services/flask/website/static/print_qr.js
+++ services/flask/website/static/print_qr.js
@@ -1,8 +1,18 @@
-function clickEvent() {
-    let myDiv = document.getElementById("qr-div");
-    let myDivContent = myDiv.innerHTML;
+// Function to show the selected QR code
+function showQR(customerName) {
+    const qrImage = document.getElementById("selected-qr");
+    qrImage.src = "{{ qrcode(customer_name, box_size=5) }}";
+
+    // Display the QR div
+    const qrDisplay = document.getElementById("qr-display");
+    qrDisplay.classList.remove("hidden");
+}
+
+// Function to print the selected QR code
+function printQR() {
+    let qrDisplay = document.getElementById("qr-display");
+    let qrDisplayContent = qrDisplay.innerHTML;
     let newWindow = window.open();
-    newWindow.document.body.innerHTML = myDivContent;
+    newWindow.document.body.innerHTML = qrDisplayContent;
     newWindow.print();
-    console.log("text");
 }
