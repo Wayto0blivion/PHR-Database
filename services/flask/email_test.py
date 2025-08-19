e8f46e43b1c0b6946d633179a48638945e376890
Wayto0blivion <98188354+Wayto0blivion@users.noreply.github.com>
2025-08-19 11:36:20 -0400

    Update email_test.py

--- services/flask/email_test.py
+++ services/flask/email_test.py
@@ -9,16 +9,16 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
-app.config['MAIL_USERNAME'] = 'phr.database.svr@gmail.com'
-app.config['MAIL_PASSWORD'] = 'orgrlaafxxjxwnqd'
+app.config['MAIL_USERNAME'] = 'test.svr@gmail.com'
+app.config['MAIL_PASSWORD'] = 'test_test'
 
 # Yahoo settings (denied connection to remote host)
 # app.config['MAIL_SERVER'] = 'smtp.mail.yahoo.com'
 # app.config['MAIL_PORT'] = 465
 # app.config['MAIL_USE_TLS'] = False
 # app.config['MAIL_USE_SSL'] = True
-# app.config['MAIL_USERNAME'] = 'phr_database_svr@yahoo.com'
-# app.config['MAIL_PASSWORD'] = 'sc@ryCave65'
+# app.config['MAIL_USERNAME'] = 'test_svr@yahoo.com'
+# app.config['MAIL_PASSWORD'] = 'uglyhorse65'
 
 
 mail = Mail(app)
